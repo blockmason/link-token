7a78d48409dcf6a5744164912d19afa6cbc08d69
Devin Canterberry <devin@canterberry.cc>
2019-02-06 09:43:48 -0800

    🎨 Add linting for Solidity code + formatting changes only

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,22 +1,27 @@
 pragma solidity 0.5.3;
 
+
 contract Migrations {
-  address public owner;
+    address public owner;
 
-  uint public last_completed_migration;
+    // Override due to Truffle toolchain requirement
+    // solhint-disable-next-line var-name-mixedcase
+    uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
+    constructor() public {
+        owner = msg.sender;
+    }
 
-  function setCompleted(uint completed) public {
-    require(msg.sender == owner);
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint completed) public {
+        require(msg.sender == owner);
+        last_completed_migration = completed;
+    }
 
-  function upgrade(address new_address) public {
-    require(msg.sender == owner);
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    // Override due to Truffle toolchain requirement
+    // solhint-disable-next-line func-param-name-mixedcase
+    function upgrade(address new_address) public {
+        require(msg.sender == owner);
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
